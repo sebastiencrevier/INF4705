1913ab65530f34b59950982d4f8e62cb2e2d02c6
Sébastien Crevier <sebastien.crevier@gsoft.com>
2017-02-20 14:07:52 -0500

    Graphs corrected

--- TP1/createGraphs.R
+++ TP1/createGraphs.R
@@ -14,36 +14,27 @@ series=1:3
 for (i in series){ 
   temp <- counting_sort[counting_sort$serie==series[i],]
   if(is.data.frame(temp) && nrow(temp)!=0){
-    
-    titre=paste("Counting sort - S",i,sep='')
+    titre=paste("Counting sort - Test constantes - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
     lm = lm(TempsMoyen ~ N, data=temp)
-    with(temp, plot((TempsMoyen) ~ (N)))
+    with(temp, plot(N, TempsMoyen))
     abline(lm, col="blue")
+    text(0,0, coef(lm)[2], col = "blue", pos=4, offset=2)
     title(main = titre)
     dev.off()
-    
+
     titre=paste("Counting sort - Test puissance - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen) ~ (N),log="xy"))
+    with(temp, plot(N, TempsMoyen),log="xy")
     title(main = titre)
     dev.off()
     
     titre= paste("Counting sort - Test rapport - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen/N) ~ (N)))
+    with(temp, plot(N, TempsMoyen/N))
     title(main = titre)
     dev.off()
-    
-    #TODO : j'arrive pas a trouver comment implémenter ce test la
-    #titre="Counting sort - Serie 1 - Test Constantes"
-    #jpeg(titre)
-    #with(temp, plot((f(TempsMoyen)) ~ (N)))
-    #title(main = titre)
-    #dev.off()
-    
   }
-
 }
 
 ### QUICK SORT FP ###
@@ -52,43 +43,42 @@ series=1:3
 for (i in 1:3){ 
   temp <- quicksort_fp[quicksort_fp$serie==series[i],]
   if(is.data.frame(temp) && nrow(temp)!=0){
-    
-    titre=paste("Quicksort FP - S",i,sep='')
+    titre=paste("Quicksort FP - Test constantes meilleur cas-cas moyen - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    lm = lm(TempsMoyen ~ N, data=temp)
-    with(temp, plot((TempsMoyen) ~ (N)))
+    lm = lm(TempsMoyen ~ I(N*log(N)), data=temp)
+    with(temp, plot(N*log(N), TempsMoyen))
     abline(lm, col="blue")
+    text(0,0, coef(lm)[2], col = "blue", pos=4, offset=2)
+    title(main = titre)
+    dev.off()
+    
+    titre=paste("Quicksort FP - Test constantes pire cas - S",i,sep='')
+    jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
+    lm = lm(TempsMoyen ~ I(N^2), data=temp)
+    with(temp, plot(N^2, TempsMoyen))
+    abline(lm, col="blue")
+    text(0,0, coef(lm)[2], col = "blue", pos=4, offset=2)
     title(main = titre)
     dev.off()
     
     titre=paste("Quicksort FP - Test puissance - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen) ~ (N),log="xy"))
+    with(temp, plot(N, TempsMoyen,log="xy"))
     title(main = titre)
     dev.off()
     
     titre= paste("Quicksort FP - Test rapport meilleur cas-cas moyen - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen/(N*log(N))) ~ (N)))
+    with(temp, plot(N, TempsMoyen/(N*log(N))))
     title(main = titre)
     dev.off()
     
     titre= paste("Quicksort FP - Test rapport pire cas - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen/(N^2)) ~ (N)))
+    with(temp, plot(N, TempsMoyen/(N^2)))
     title(main = titre)
-    dev.off()
-    
-    
-    #TODO : j'arrive pas a trouver comment implémenter ce test la
-    #titre="Counting sort - Serie 1 - Test Constantes"
-    #jpeg(titre)
-    #with(temp, plot((f(TempsMoyen)) ~ (N)))
-    #title(main = titre)
-    #dev.off()
-    
+    dev.off()    
   }
-  
 }
 
 ### QUICK SORT FP T ###
@@ -97,43 +87,42 @@ series=1:3
 for (i in series){ 
   temp <- quicksort_fp_t[quicksort_fp_t$serie==series[i],]
   if(is.data.frame(temp) && nrow(temp)!=0){
-    
-    titre=paste("Quicksort FP seuil - S",i,sep='')
+    titre=paste("Quicksort FP seuil - Test constantes meilleur cas-cas moyen - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    lm = lm(TempsMoyen ~ N, data=temp)
-    with(temp, plot((TempsMoyen) ~ (N)))
+    lm = lm(TempsMoyen ~ I(N*log(N)), data=temp)
+    with(temp, plot(N*log(N), TempsMoyen))
     abline(lm, col="blue")
+    text(0,0, coef(lm)[2], col = "blue", pos=4, offset=2)
+    title(main = titre)
+    dev.off()
+    
+    titre=paste("Quicksort FP seuil - Test constantes pire cas - S",i,sep='')
+    jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
+    lm = lm(TempsMoyen ~ I(N^2), data=temp)
+    with(temp, plot(N^2, TempsMoyen))
+    abline(lm, col="blue")
+    text(0,0, coef(lm)[2], col = "blue", pos=4, offset=2)
     title(main = titre)
     dev.off()
     
     titre=paste("Quicksort FP seuil - Test puissance - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen) ~ (N),log="xy"))
+    with(temp, plot(N, TempsMoyen,log="xy"))
     title(main = titre)
     dev.off()
     
     titre= paste("Quicksort FP seuil - Test rapport meilleur cas-cas moyen - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen/(N*log(N))) ~ (N)))
+    with(temp, plot(N, TempsMoyen/(N*log(N))))
     title(main = titre)
     dev.off()
     
     titre= paste("Quicksort FP seuil - Test rapport pire cas - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen/(N^2)) ~ (N)))
+    with(temp, plot(N, TempsMoyen/(N^2)))
     title(main = titre)
-    dev.off()
-  
-    
-    #TODO : j'arrive pas a trouver comment implémenter ce test la
-    #titre="Counting sort - Serie 1 - Test Constantes"
-    #jpeg(titre)
-    #with(temp, plot((f(TempsMoyen)) ~ (N)))
-    #title(main = titre)
-    #dev.off()
-    
+    dev.off()    
   }
-  
 }
 
 ### QUICK SORT RP ###
@@ -142,43 +131,42 @@ series=1:3
 for (i in series){ 
   temp <- quicksort_rp[quicksort_rp$serie==series[i],]
   if(is.data.frame(temp) && nrow(temp)!=0){
-    
-    titre=paste("Quicksort RP - S",i,sep='')
+    titre=paste("Quicksort RP - Test constantes meilleur cas-cas moyen - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    lm = lm(TempsMoyen ~ N, data=temp)
-    with(temp, plot((TempsMoyen) ~ (N)))
+    lm = lm(TempsMoyen ~ I(N*log(N)), data=temp)
+    with(temp, plot(N*log(N), TempsMoyen))
     abline(lm, col="blue")
+    text(0,0, coef(lm)[2], col = "blue", pos=4, offset=2)
+    title(main = titre)
+    dev.off()
+    
+    titre=paste("Quicksort RP - Test constantes pire cas - S",i,sep='')
+    jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
+    lm = lm(TempsMoyen ~ I(N^2), data=temp)
+    with(temp, plot(N^2, TempsMoyen))
+    abline(lm, col="blue")
+    text(0,0, coef(lm)[2], col = "blue", pos=4, offset=2)
     title(main = titre)
     dev.off()
     
     titre=paste("Quicksort RP - Test puissance - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen) ~ (N),log="xy"))
+    with(temp, plot(N, TempsMoyen,log="xy"))
     title(main = titre)
     dev.off()
     
     titre= paste("Quicksort RP - Test rapport meilleur cas-cas moyen - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen/(N*log(N))) ~ (N)))
+    with(temp, plot(N, TempsMoyen/(N*log(N))))
     title(main = titre)
     dev.off()
     
     titre= paste("Quicksort RP - Test rapport pire cas - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen/(N^2)) ~ (N)))
+    with(temp, plot(N, TempsMoyen/(N^2)))
     title(main = titre)
-    dev.off()
-
-    
-    #TODO : j'arrive pas a trouver comment implémenter ce test la
-    #titre="Counting sort - Serie 1 - Test Constantes"
-    #jpeg(titre)
-    #with(temp, plot((f(TempsMoyen)) ~ (N)))
-    #title(main = titre)
-    #dev.off()
-    
+    dev.off()   
   }
-  
 }
 
 ### QUICK SORT RP T ###
@@ -187,42 +175,40 @@ series=1:3
 for (i in series){ 
   temp <- quicksort_rp_t[quicksort_rp_t$serie==series[i],]
   if(is.data.frame(temp) && nrow(temp)!=0){
-    
-    titre=paste("Quicksort RP seuil - S",i,sep='')
+    titre=paste("Quicksort RP seuil - Test constantes meilleur cas-cas moyen - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    lm = lm(TempsMoyen ~ N, data=temp)
-    with(temp, plot((TempsMoyen) ~ (N)))
-    abline(lm,col="blue")
+    lm = lm(TempsMoyen ~ I(N*log(N)), data=temp)
+    with(temp, plot(N*log(N), TempsMoyen))
+    abline(lm, col="blue")
+    text(0,0, coef(lm)[2], col = "blue", pos=4, offset=2)
+    title(main = titre)
+    dev.off()
+    
+    titre=paste("Quicksort RP seuil - Test constantes pire cas - S",i,sep='')
+    jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
+    lm = lm(TempsMoyen ~ I(N^2), data=temp)
+    with(temp, plot(N^2, TempsMoyen))
+    abline(lm, col="blue")
+    text(0,0, coef(lm)[2], col = "blue", pos=4, offset=2)
     title(main = titre)
     dev.off()
     
     titre=paste("Quicksort RP seuil - Test puissance - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen) ~ (N),log="xy"))
+    with(temp, plot(N, TempsMoyen,log="xy"))
     title(main = titre)
     dev.off()
     
     titre= paste("Quicksort RP seuil - Test rapport meilleur cas-cas moyen - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen/(N*log(N))) ~ (N)))
+    with(temp, plot(N, TempsMoyen/(N*log(N))))
     title(main = titre)
     dev.off()
     
     titre= paste("Quicksort RP seuil - Test rapport pire cas - S",i,sep='')
     jpeg(paste(getwd(),"/results/",titre,'.jpg',sep=''))
-    with(temp, plot((TempsMoyen/(N^2)) ~ (N)))
+    with(temp, plot(N, TempsMoyen/(N^2)))
     title(main = titre)
-    dev.off()
-    
-    #TODO : j'arrive pas a trouver comment implémenter ce test la
-    #titre="Counting sort - Serie 1 - Test Constantes"
-    #jpeg(titre)
-    #with(temp, plot((f(TempsMoyen)) ~ (N)))
-    #title(main = titre)
-    #dev.off()
-    
+    dev.off()   
   }
-  
 }
-
-
