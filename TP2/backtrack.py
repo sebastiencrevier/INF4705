3f08d3b1972f793bda96c2bfd36893cec7a884b2
Sébastien Crevier <sebastien.crevier@gsoft.com>
2017-03-15 12:24:35 -0400

    backtrack algo working

--- TP2/backtrack.py
+++ TP2/backtrack.py
@@ -1,5 +1,31 @@
-def backtrack():
-    print(5)
+import networkx as nx
+
+def backtrack_recursive(G, ext):
+    exts = []
+
+    for n in G.nodes():
+        if G.edges() and not G.successors(n) and G.predecessors(n):
+            continue
+
+        temp = ext.copy()
+        temp.append(n)
+
+        g = G.copy()
+        g.remove_node(n)
+
+        result = backtrack_recursive(g, temp)
+
+        for r in result:
+            exts.append(r)
+
+        if len(G.nodes()) == 1:
+            exts.append(temp)
+
+    return exts
 
 if __name__ == "__main__":
-    backtrack()
+    G = nx.DiGraph()
+    G.add_edges_from([(1,3), (2,3), (2,0)])
+
+    nodes = backtrack_recursive(G, [])
+    print(nodes)
